bde531d8e58079ffeeeffdf1645a156f49dfdc94
G7M7T1 <genlisense@gmail.com>
2022-02-27 17:43:14 -0500

    Update FIle

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 import './style.scss'
+
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector("body").classList.add("display");
+    }, 4000);
+};
+
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change");
+});
+
+document.querySelector(".scroll-btn").addEventListener("click", () => {
+    document.querySelector("html").style.scrollBehavior = "smooth";
+    setTimeout(() => {
+        document.querySelector("html").style.scrollBehavior = "unset";
+    }, 1000);
+});
